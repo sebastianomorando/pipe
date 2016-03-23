222ff93028afc8054ae2375dd59b8d52e9b40fba
Sebastiano Morando <sebastianomorando@gmail.com>
2016-03-23 13:38:38 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sebastiano Morando</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<link href="<?php echo get_stylesheet_uri(); ?>" rel="stylesheet" type="text/css" >
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-24380560-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</head>
+
+<body>
+
+  <a href="http://www.atraxmedia.net/" target="_blank">
+    <div id="centered">
+      <span class="icon-pipe big"> </span>
+      <div id="ceci"> Ceci n'est pas un site. </div>
+    </div>
+  </a>
+  
+  <section id="social">
+    <a href="https://twitter.com/dettofernet" alt="Sebastiano Morando twitter" target="_blank"><i class="fa fa-twitter-square fa-4x"></i></a>
+    <a href="https://instagram.com/mastornadettofernet/" alt="Sebastiano Morando instagram" target="_blank">  <i class="fa fa-instagram fa-4x"></i></a>
+    <a href="https://it.linkedin.com/pub/sebastiano-morando/71/197/a99" alt="Sebastiano Morando linkedin" target="_blank">  <i class="fa fa-linkedin-square fa-4x"></i></a>
+    <a href="mailto:sebastianomorando@gmail.com" alt="Sebastiano Morando email" target="_blank"> <i class="fa fa-envelope-square fa-4x"></i> </a>
+    <a href="https://www.youtube.com/user/mastornadettofernet" alt="Sebastiano Morando Youtube" target="_blank"> <i class="fa fa-youtube-square fa-4x"></i> </a>
+    <a href="skype:sebastianomorando?call" alt="Sebastiano Morando Skype" target="_blank"> <i class="fa fa-skype fa-4x"></i> </a>
+  </section>
+  
+  <section id="dati">
+    Sebastiano Morando <br> web developer <br> P.IVA 03977660236
+  </section>
+  
+</body>
+</html>
